0cb0c0859f3f62a30b828e479ae90ee2b845982c
unknown <guruvihar2022@gmail.com>
2024-03-10 02:28:32 +0530

    initial commit

---
+++ src/main/java/com/fileupload/uploaddemo/UploaddemoApplication.java
@@ -0,0 +1,25 @@
+package com.fileupload.uploaddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fileupload.uploaddemo.service.FilesStorageService;
+
+import jakarta.annotation.Resource;
+
+@SpringBootApplication
+public class UploaddemoApplication implements CommandLineRunner{
+
+	  @Resource
+	  FilesStorageService storageService;
+
+	  public static void main(String[] args) {
+	    SpringApplication.run(UploaddemoApplication.class, args);
+	  }
+
+	  @Override
+	  public void run(String... arg) throws Exception {
+	    storageService.init();
+	  }
+}
---
+++ src/main/java/com/fileupload/uploaddemo/message/ResponseMessage.java
@@ -0,0 +1,21 @@
+package com.fileupload.uploaddemo.message;
+
+public class ResponseMessage {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public ResponseMessage(String message) {
+		super();
+		this.message = message;
+	}
+	
+	
+
+}
